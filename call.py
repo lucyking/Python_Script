89813e14b9e7a71b4aaa9af9098865af50ea3954
lucyking <joehisaishi1943@gmail.com>
2016-04-29 15:11:55 +0800

    + test files

--- call.py
+++ call.py
@@ -1,24 +1,24 @@
-from  main  import MB,B
+from  main import MB, B
 import re
 import os
 import shutil
+
 kk = MB()
 # kk.multi(5, 6)
 # MB.multi(5,6)
-kk.multi(2,9)
-kk.sub(9,2)
-kk.sub(2,9)
-s = re.search('click','click input')
-
+kk.multi(2, 9)
+kk.sub(9, 2)
+kk.sub(2, 9)
+s = re.search('click', 'click input')
 
 print '[-------s333333333------------]'
-s2 = re.search('[iI]nput|[cC]lick','Input input Click click')
+s2 = re.search('[iI]nput|[cC]lick', 'Input input Click click')
 print s2
 if s2:
     print s2.string
 
 print '[-------s333333333------------]'
-s3 = re.search(r'[iI]nput|[cC]lick','Input Click ')
+s3 = re.search(r'[iI]nput|[cC]lick', 'Input Click ')
 print s3
 if s3:
     print s3.string
@@ -26,19 +26,20 @@ if s3:
 
 current_dir = os.path.abspath('.')
 if os.path.isdir('test'):
-	shutil.rmtree('test')
+    shutil.rmtree('test')
 os.mkdir('test')
-my_dir =  os.path.join(current_dir,'test')
-my_dir =  os.path.join(my_dir,'test.txt')
-print '[>>>]my dir',my_dir
+my_dir = os.path.join(current_dir, 'test')
+my_dir = os.path.join(my_dir, 'test.txt')
+print '[>>>]my dir', my_dir
 shutil.rmtree('test')
 
 for item in os.listdir(current_dir):
-    if os.path.splitext(item)[1] == '.png' or os.path.splitext(item)[1] =='.gif':
-        print item
+    print item
+    if os.path.splitext(item)[1] == '.png' or os.path.splitext(item)[1] == '.gif':
+        print '[png/gif]:',item
 
-        # print os.path.abspath(item)
-        # print current_dir+'\\'+item
-        # print current_dir+'\\'+current_test+'\\'+item
-        # shutil.move(current_output_dir+'\\'+item,current_output_dir+'\\'+name+'\\'+item)
+    # print os.path.abspath(item)
+    # print current_dir+'\\'+item
+    # print current_dir+'\\'+current_test+'\\'+item
+    # shutil.move(current_output_dir+'\\'+item,current_output_dir+'\\'+name+'\\'+item)
 print '----------------------------------------------'
